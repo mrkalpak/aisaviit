fdfb7e4b200e45a7c8c9cf533a92b1f6d8fc4256
Sarthak5290 <123548573+Sarthak5290@users.noreply.github.com>
2023-02-15 02:50:27 +0530

    data for Event.js file

---
+++ src/data/event.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    key: 1,
+    title: "Title Name",
+    description:
+      "Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et",
+    image: require("../media/img/Logo_18.jpeg"),
+    href: "/",
+  },
+  {
+    key: 2,
+    title: "third Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et",
+      image: require("../media/img/Logo_18.jpeg"),
+      href: "/",
+  },
+  {
+    key: 3,
+    title: "New Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et",
+      image: require("../media/img/Logo_18.jpeg"),
+      href: "/",
+  },
+];
+
+export default data;
