61416a5a5fd520dfbfce2bff551445cb8e5ffd4c
Sarthak5290 <123548573+Sarthak5290@users.noreply.github.com>
2023-02-15 02:46:08 +0530

    Updating Event.js file

--- src/component/Event.js
+++ src/component/Event.js
@@ -1,11 +1,31 @@
 import React from "react";
 import './css/home.css'
+import "./css/Event.css";
+import data from "../data/event";
+
 const Event = () => {
+  return data.map((element) => {
     return (
-      <div className='App'>
-        <h1 className="text-center">Event</h1>
+      <div className="App">
+        <hr />
+        <h1 className="text-center eventName">{element.title}</h1>
+        <div className="event">
+          <div className="card mb-3 image">
+            <img src={element.image} className="card-img-top" alt="..." />
+            <div className="card-body detail">
+              <h5 className="card-title title">
+                <h1>{element.title}</h1>
+              </h5>
+              <p className="card-text description">
+                {element.description} <a href={element.href}>learn more...</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <hr />
       </div>
-    )
-  }
-  
+    );
+  });
+};
+
   export default Event
