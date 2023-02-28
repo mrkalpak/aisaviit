2277a08c682ddcc57c61d922e39e9df3b761a47f
kalpak nemade <61384122+mrkalpak@users.noreply.github.com>
2023-02-28 10:38:29 +0530

    about

--- src/component/About.js
+++ src/component/About.js
@@ -1,11 +1,18 @@
 import React from "react";
 // import "./css/about.css"
+import logo from "../media/img/aisalogo.png" 
 
 const About = () => {
     return (
         <div>
             <div className="text-center">
-
+            <h1 className="text-center">About Us</h1>
+                <img src={logo} className="w-25" alt=""/>
+                <br/>
+                <h3>ARTIFICIAL INTELLEGINCE STUDENTS ASSOCIATION</h3>
+                <div className="h5">
+                AISA is an association of students forging an awareness of the newest AI technologies and concepts. <br/> We aim to lead the foundation of a perspective that broadens the thought of AI and humans walking hand-in-hand.
+                </div>
             </div>
         </div>
     );
